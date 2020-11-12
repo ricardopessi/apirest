8621a3bd2d9d4fcd1d4d3fc3a3d32c3aef4ad3f4
Naldo Colella <naldo.colella@gmail.com>
2020-11-12 20:46:30 -0300

    Initial commit

---
+++ src/main/java/com/crudprodutos/apirest/ApirestApplication.java
@@ -0,0 +1,13 @@
+package com.crudprodutos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crudprodutos/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.crudprodutos.apirest.repository;
+
+import com.crudprodutos.apirest.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
