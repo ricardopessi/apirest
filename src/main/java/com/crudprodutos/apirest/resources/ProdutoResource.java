c72cec947e395b02821fba9ebf869949511e20cb
Naldo Colella <naldo.colella@gmail.com>
2020-11-12 22:50:24 -0300

    Conf for Swagger

--- src/main/java/com/crudprodutos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/crudprodutos/apirest/resources/ProdutoResource.java
@@ -2,6 +2,8 @@ package com.crudprodutos.apirest.resources;
 
 import com.crudprodutos.apirest.models.Produto;
 import com.crudprodutos.apirest.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,32 +11,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API Rest Produtos")
+@CrossOrigin(origins="*")
 public class ProdutoResource {
 
     @Autowired
     ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos")
+    @ApiOperation(value="Retorna uma lista de produtos")
     public List<Produto> getList(){
         return produtoRepository.findAll();
     }
 
     @GetMapping("/produtos/{id}")
+    @ApiOperation(value="Retorna um produto a partir da id")
     public Produto getById(@PathVariable(value="id") long id){
         return produtoRepository.findById(id);
     }
 
     @PostMapping("/produto")
+    @ApiOperation(value="Cria um Produto")
     public Produto insert(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
 
     @DeleteMapping("/produto")
+    @ApiOperation(value="Deleta um produto")
     public void delete(@RequestBody Produto produto){
         produtoRepository.delete(produto);
     }
 
     @PutMapping("/produto")
+    @ApiOperation(value="Atualiza um produto")
     public Produto update(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
